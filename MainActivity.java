e4ac9f4848a3eb17df57fa1a62d2f5b8a4db7fd8
Anto Joseph <antojoseph007@gmail.com>
2016-02-23 17:05:41 +0530

    Test App which uses WebView loadUrl Method
    Test

---
+++ MainActivity.java
@@ -0,0 +1,31 @@
+package com.jni.anto.kalip;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.webkit.WebView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        WebView w = (WebView) findViewById(R.id.webView);
+        w.loadUrl("http://www.frida.re/");
+        w.getSettings().setJavaScriptEnabled(true);
+
+                }
+            }
+
+
+
+
+
+
